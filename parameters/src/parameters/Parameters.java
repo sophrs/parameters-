340a956faa37bf2d32b72f09b554b1f62a1fd856
sophrs <45198009+sophrs@users.noreply.github.com>
2018-11-21 13:49:00 +0000

    Add files via upload

---
+++ parameters/src/parameters/Parameters.java
@@ -0,0 +1,12 @@
+package parameters;
+
+public class Parameters {
+	public static void main(String[] args) {
+		helloWorld("Hello World!");
+	}
+	
+	public static void helloWorld(String hello) {
+		System.out.println(hello);
+	}
+
+}
